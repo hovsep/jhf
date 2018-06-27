bf57c8428791623a63e5bb05cd770b212cfe56d1
Hovsep <avakyan.hovsep@yandex.ru>
2018-06-27 16:16:41 +0300

    Chapter2: Dog task

---
+++ src/chapter2/Dog.java
@@ -0,0 +1,24 @@
+package chapter2;
+
+public class Dog {
+    int size;
+
+    String breed;
+    String name;
+
+    void bark(){
+        System.out.println("Ruff! Ruff!");
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
