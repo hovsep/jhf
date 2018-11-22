0354b346d0142a675f0cd1ec51d787ee94c71764
Hovsep <avakyan.hovsep@yandex.ru>
2018-11-22 15:09:27 +0300

    io

--- src/Sandbox.java
+++ src/Sandbox.java
@@ -1,9 +1,28 @@
 
 import java.util.Scanner;
 
-public class Sandbox extends Object {
+public class Sandbox {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        if (args.length < 2) {
+            throw new Exception("Too few params");
+        }
+
+        int a  = Integer.parseInt(args[0]);
+        int b  = Integer.parseInt(args[1]);
+
+        if (a == 0) {
+            System.out.println(b);
+        }
+
+        if (b == 0) {
+            System.out.println(a);
+        }
+
+        if (a >= b) {
+
+        }
+
 
     }
 
---
+++ src/chapter14/io/App.java
@@ -0,0 +1,67 @@
+package chapter14.io;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+public class App {
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.run();
+    }
+
+
+    private void run()
+    {
+        Hova h = new Hova(27, "Ёхри-мохри");
+        Vita v = new Vita(23, "Кутусьма");
+
+        try {
+            FileOutputStream fstream = new FileOutputStream("/home/hovsep/tmp/jhf.data");
+            ObjectOutputStream ostream = new ObjectOutputStream(fstream);
+
+            ostream.writeObject(h);
+            ostream.writeObject(v);
+            ostream.close();
+        } catch (Exception e) {
+            System.out.println("Ooops. Reason: " + e.getMessage());
+        }
+
+
+    }
+
+
+    class Hova {
+
+        private int age = 27;
+
+        private String name = "Hovsep";
+
+        public Hova(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+        private String makeAWish()
+        {
+            return "Cool job";
+        }
+    }
+
+    class Vita {
+
+        private int age = 23;
+
+        private String name = "Vita";
+
+        public Vita(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+        private String makeAWish()
+        {
+            return "Happy life";
+        }
+    }
+}
