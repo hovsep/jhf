247945fa74e46b918cb16a024fd8c622a85f90a0
Hovsep <avakyan.hovsep@yandex.ru>
2018-11-27 22:07:24 +0300

    sandbox

--- src/Sandbox.java
+++ src/Sandbox.java
@@ -4,27 +4,16 @@ import java.util.Scanner;
 
 public class Sandbox implements Serializable {
 
-    public static void main(String[] args) throws Exception {
-        if (args.length < 2) {
-            throw new Exception("Too few params");
-        }
+    private static int B;
 
-        int a  = Integer.parseInt(args[0]);
-        int b  = Integer.parseInt(args[1]);
-
-        if (a == 0) {
-            System.out.println(b);
-        }
-
-        if (b == 0) {
-            System.out.println(a);
-        }
-
-        if (a >= b) {
-
-        }
+    public static void main(String[] args) {
 
 
+        Scanner scanner = new Scanner(System.in);
+
+        String line = scanner.nextLine();
+
+        System.out.println(line);
     }
 
 }
